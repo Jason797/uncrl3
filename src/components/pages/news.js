70e79e88dd9f54d952b452eb0b2ab310965f8b80
Jason797 <jason797@live.unc.edu>
2021-05-06 04:17:25 -0400

    fetch

--- src/components/pages/news.js
+++ src/components/pages/news.js
@@ -2,13 +2,9 @@
 
 function News() {
 
-    const http = new XMLHttpRequest();
     const url= 'http://api.steampowered.com/ISteamNews/GetNewsForApp/v0002/?appid=440&count=3&maxlength=300&format=json'
-    http.open("GET", url)
-    http.send()
-    http.onreadystatechange = (e) => {
-        console.log(http.responseText)
-      }
+    fetch(url)
+    .then(data=>{console.log(data)})
 
     return (
         <div className="container-fluid">
